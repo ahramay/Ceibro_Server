10a64c4c9a38d9c9801a8c41c7f8e5bcd81750cc
ahramay <ahmad.ramay4@gmail.com>
2022-10-05 01:45:15 -0700

    Ceibro_Server

---
+++ src/models/work.model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const { toJSON, paginate } = require('./plugins');
+
+const workSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    locations: [
+      {
+        type: String,
+        required: true,
+        trim: true,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// add plugin that converts mongoose to json
+workSchema.plugin(toJSON);
+workSchema.plugin(paginate);
+
+/**
+ * @typedef Work
+ */
+const Work = mongoose.model('Work', workSchema);
+
+module.exports = Work;
